0880a5f8c22db610f2bd64291682567c24a69f70
GP Sin <GPSins9779@gmail.com>
2025-04-27 13:04:52 -0500

    removed /api from url

--- Pearson_World_Journal/frontend/src/pages/api.ts
+++ Pearson_World_Journal/frontend/src/pages/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://world-journal.onrender.com/api',
+  baseURL: 'https://world-journal.onrender.com',
 });
 
 export default API;
