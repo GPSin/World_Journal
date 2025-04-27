b9e5012b5c10ce8093ec2f20ac25b1f6c0654b90
GP Sin <GPSins9779@gmail.com>
2025-04-27 00:05:50 -0500

    updated cors in server.js

--- Pearson_World_Journal/backend/server.js
+++ Pearson_World_Journal/backend/server.js
@@ -87,7 +87,10 @@ function saveWaypoints(data) {
 
 let waypoints = loadWaypoints();
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://world-journal.vercel.app/'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
